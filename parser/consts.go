ea872c53ba0bf44f16f109a2de00e341ea354b20
Jeremy Keeler <jeremy@keeler.dev>
2025-01-18 09:35:04 -0500

    Bump version to 0.1.8

--- parser/consts.go
+++ parser/consts.go
@@ -4,7 +4,7 @@ const OUTER_HIERARCHY_START_OFFSET = 257
 const MAX_SCAN_OFFSET = 50
 const DATA_OFFSET = 6
 const ROOT_NODE_TOKEN = "BG"
-const VERSION = "v0.1.4"
+const VERSION = "v0.1.8"
 
 var FOOTER = []uint8{0, 1, 0, 0, 0, 0, 0, 0}
 
