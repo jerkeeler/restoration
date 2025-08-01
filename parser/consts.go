77008328b008dd3b52dce50bc1539ca83b6287a5
Jeremy Keeler <jeremy@keeler.dev>
2025-08-01 10:36:49 -0400

    Bump to version 0.4.0

--- parser/consts.go
+++ parser/consts.go
@@ -3,7 +3,7 @@ package parser
 const MAX_SCAN_OFFSET = 50
 const DATA_OFFSET = 6
 const ROOT_NODE_TOKEN = "BG"
-const VERSION = "v0.3.1"
+const VERSION = "v0.4.0"
 
 var FOOTER = []uint8{0x19, 0x0, 0x0, 0x0}
 
